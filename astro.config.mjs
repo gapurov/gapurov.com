1e23f8513230114e0b0b93ac3929c21aa68560e9
Vladislav Gapurov <gapurov@gmail.com>
2023-04-19 12:52:22 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
 
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.gapurov.com/',
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind()],
 })
