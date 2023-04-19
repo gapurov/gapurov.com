46f1122938748372c247a4622a7d860b4f6fcba5
Vladislav Gapurov <gapurov@gmail.com>
2023-04-19 12:47:03 +0200

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gapurov.com/',
+  site: 'https://www.gapurov.com/',
   integrations: [mdx(), tailwind()],
 })
